5e923afded400aad240e241f410f37b9b1d2e78c
Le Quang Tri <71906481+leqtr@users.noreply.github.com>
2025-01-03 05:18:41 +0800

    Update schemas.py

--- service/schemas.py
+++ service/schemas.py
@@ -1,48 +1,53 @@
-from pydantic import BaseModel
-from typing import List, Dict, Optional, Union, Any
-from enum import Enum
+from pydantic import BaseModel, Field
+from typing import List, Dict, Union, Any
 
-
-class Vacancy(BaseModel):
+class Vacancy_for_fit(BaseModel):
     name: str
     area_name: str
+    employer_name: str
     schedule: str
     employment: str
     experience: str
-    professional_roles_name: str
-    count_key_skills: int
-    salary_from: Optional[Union[int, float]] = None
-    salary_to: Optional[Union[int, float]] = None
+    # professional_roles_name: str
+    # count_key_skills: int
+    salary_from: Union[int, float] = None
+    salary_to: Union[int, float] = None
 
+class Vacancy_for_predict(BaseModel):
+    name: str
+    area_name: str
+    employer_name: str
+    schedule: str
+    employment: str
+    experience: str
 
-class Vacancies(BaseModel):
-    data: List[Vacancy]
-
-
-class ModelType(str, Enum):
-    linear = "linear"
-
-
-class ModelConfig(BaseModel):
-    id: str
-    description: Optional[str] = None
-    ml_model_type: ModelType
-    hyperparameters: Dict[str, Any]
-
+class FitConfig(BaseModel):
+    model_id: str = Field(..., description="Unique identifier for the model")
+    # description: Optional[str] = None
+    # ml_model_type: str
+    hyperparameters: Dict[str, Any] = Field(..., description="Hyperparameters for the model")
 
 class FitRequest(BaseModel):
-    data: Vacancies
-    config: ModelConfig
-
+    data: List[Vacancy_for_fit] = Field(..., description="Training data for the model")
+    config: FitConfig = Field(..., description="Configuration for the model training")
 
 class FitResponse(BaseModel):
-    message: str
+    message: str = Field(..., description="Message confirming the training status of the model")
 
-
+###
 class PredictRequest(BaseModel):
-    model_id: str
-    data: Vacancies
+    model_id: str = Field(..., description="ID of the model to use for prediction")
+    data: List[Vacancy_for_predict] = Field(..., description="Data for which predictions are required")
 
+class PredictResponse(BaseModel):
+    predictions: List[float] = Field(..., description="Predictions made by the model")
 
-class PredictionResponse(BaseModel):
-    predictions: List[float]
+###
+class ModelListResponse(BaseModel):
+    models: List[Dict[str, str]] = Field(..., description="List of available models")
+
+### set
+class SetRequest(BaseModel):
+    model_id: str = Field(..., description="ID of the model to set as active")
+class SetResponse(BaseModel):
+    message: str = Field(..., description="Message confirming the model is set as active")
